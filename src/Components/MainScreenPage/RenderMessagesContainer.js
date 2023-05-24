d6ea5e0bb01b147024fe408598c372fb5d7c147d
EduuCarvalho <educarvalho.engcivil@gmail.com>
2023-05-24 19:20:12 -0300

    feat: Fixed Post messages contaqiner Done

---
+++ src/Components/MainScreenPage/RenderMessagesContainer.js
@@ -0,0 +1,9 @@
+
+
+export default function RenderMessagesContainer() {
+
+    return (
+        <>Olaa
+        </>
+    );
+}
--- src/Pages/MainScreen.js
+++ src/Pages/MainScreen.js
@@ -1,9 +1,23 @@
-
+import styled from "styled-components";
+import FixedPostMessageContainer from "../Components/MainScreenPage/FixedPostMessageContainer";
+import RenderMessagesContainer from "../Components/MainScreenPage/RenderMessagesContainer";
 
 export default function MainScreen() {
 
    return(
-        <>
-        </>
+        <MainPage> 
+            <FixedPostMessageContainer/>
+            <RenderMessagesContainer/>
+        
+
+        </MainPage>
     );
 };
+
+const MainPage = styled.div`
+    width:100vw;
+    height:100vh;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+`
