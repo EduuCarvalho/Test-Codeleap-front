82174989410eaa2665da03c6e314a64e9f7c6aea
EduuCarvalho <educarvalho.engcivil@gmail.com>
2023-05-24 20:09:38 -0300

    feat: static app done

--- src/Components/MainScreenPage/FixedPostMessageContainer.js
+++ src/Components/MainScreenPage/FixedPostMessageContainer.js
@@ -1,135 +1,155 @@
-import styled from "styled-components"
-
+import styled from "styled-components";
+import { SlArrowUp, SlArrowDown } from "react-icons/sl";
+import { useState } from "react";
 export default function FixedPostMessageContainer() {
+  const [showPostContainer, setShowPostContainer] = useState(true);
 
-    return (
-        <MainContainer>
-            <TittleBar>
-                <h1>CodeLeap Network</h1>
-            </TittleBar>
-            <PostMessageContainer>
-                <h1>What's on your mind?</h1>
-                <TittleInputContainer>
-                    <h1>Tittle</h1>
-                    <input placeholder="Hello World"></input>
-                </TittleInputContainer>
-                <ContentInputContainer>
-                    <h1>Content</h1>
-                    <input placeholder="Content here"></input>
-                </ContentInputContainer>
-                <CreateButton>Create</CreateButton>
-            </PostMessageContainer>
-        </MainContainer>
-    )
+  return (
+    <MainContainer>
+      <TittleBar>
+        <h1>CodeLeap Network</h1>
+      </TittleBar>
+      {showPostContainer && (
+        <PostMessageContainer show={showPostContainer}>
+          <h1>What's on your mind?</h1>
+          <TittleInputContainer>
+            <h1>Tittle</h1>
+            <input placeholder="Hello World"></input>
+          </TittleInputContainer>
+          <ContentInputContainer>
+            <h1>Content</h1>
+            <input placeholder="Content here"></input>
+          </ContentInputContainer>
+          <CreateButton>Create</CreateButton>
+        </PostMessageContainer>
+      )}
+      {showPostContainer ? (
+        <ShowPostButton onClick={() => setShowPostContainer(false)}  />
+      ) : (
+        <HidePostButton onClick={() => setShowPostContainer(true)} />
+      )}
+    </MainContainer>
+  );
 }
 
 const MainContainer = styled.div`
-    width:100%;
-    height:500px;
-    display:flex;
-    flex-direction:column;
-    align-items:center;
-    box-shadow: 0px 20px 40px rgba(118, 149, 236, 0.3);
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: 0px 20px 40px rgba(118, 149, 236, 0.3);
+`;
 
-`
+const TittleBar = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 27px 37px 27px 37px;
+  background: #7695ec;
+  width: 100%;
+  margin-bottom: 30px;
 
-const TittleBar = styled.div `
-    display:flex;
-    align-items:center;
-    padding:27px 37px 27px 37px;
-    background: #7695EC;
-    width:100%;
-    margin-bottom:30px;
-
-     h1{
-        font-family: 'Roboto';
-        font-weight: 700;
-        font-size: 22px;
-        line-height: 26px;
-        font-style: normal;
-        color:#FFFFFF;
-     }
-`
-const PostMessageContainer = styled.div`
-    display:flex;
-    flex-direction:column;
-    justify-content:space-evenly;
-    align-items:flex-end;
-    padding:24px;
-    background: #FFFFFF;
-    border: 1px solid #999999;
-    border-radius: 16px;
-    width:90%;
-    height:350px;
-
-    h1{
-        font-family: 'Roboto';
-        font-style: normal;
-        font-weight: 700;
-        font-size: 22px;
-        line-height: 26px;
-        color: #000000;
-        width:100%;
-    }
-`
-const TittleInputContainer=styled.div`
-    display:flex;
-    flex-direction:column;
-    width:100%;
-
-    h1 {
-        font-family: 'Roboto';
-        font-style: normal;
-        font-weight: 400;
-        font-size: 16px;
-        line-height: 19px;
-        color: #000000;
-    }
-
-    input {
-        border: 1px solid #777777;
-        border-radius: 8px;
-        background: #FFFFFF;
-        height:32px;
-        margin-top:8px;
-        padding:8px;
-    }
-`
-const ContentInputContainer=styled.div`
-    display:flex;
-    flex-direction:column;
-    width:100%;
-
-    h1 {
-        font-family: 'Roboto';
-        font-style: normal;
-        font-weight: 400;
-        font-size: 16px;
-        line-height: 19px;
-        color: #000000;
-    }
-
-    input {
-        border: 1px solid #777777;
-        border-radius: 8px;
-        background: #FFFFFF;
-        height:74px;
-        margin-top:8px;
-        margin-bottom:20px;
-        padding:8px;
-    }
-`
-
-const CreateButton = styled.button`
-    width: 120px;
-    height: 32px;
-    background: #7695EC;
-    border-radius: 8px;
-    border:none;
-    font-family: 'Roboto';
+  h1 {
+    font-family: "Roboto";
     font-weight: 700;
+    font-size: 22px;
+    line-height: 26px;
+    font-style: normal;
+    color: #ffffff;
+  }
+`;
+const PostMessageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: flex-end;
+  padding: 24px;
+  background: #ffffff;
+  border: 1px solid #999999;
+  border-radius: 16px;
+  width: 90%;
+  height: 350px; 
+
+
+  h1 {
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 22px;
+    line-height: 26px;
+    color: #000000;
+    width: 100%;
+  }
+`;
+const TittleInputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  h1 {
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 400;
     font-size: 16px;
     line-height: 19px;
-    color: #FFFFFF;
+    color: #000000;
+  }
+
+  input {
+    border: 1px solid #777777;
+    border-radius: 8px;
+    background: #ffffff;
+    height: 32px;
+    margin-top: 8px;
+    padding: 8px;
+  }
+`;
+const ContentInputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  h1 {
+    font-family: "Roboto";
     font-style: normal;
-`
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19px;
+    color: #000000;
+  }
+
+  input {
+    border: 1px solid #777777;
+    border-radius: 8px;
+    background: #ffffff;
+    height: 74px;
+    margin-top: 8px;
+    margin-bottom: 20px;
+    padding: 8px;
+  }
+`;
+
+const CreateButton = styled.button`
+  width: 120px;
+  height: 32px;
+  background: #7695ec;
+  border-radius: 8px;
+  border: none;
+  font-family: "Roboto";
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 19px;
+  color: #ffffff;
+  font-style: normal;
+`;
+
+const ShowPostButton = styled(SlArrowUp)`
+  margin-bottom: 20px;
+  margin-top: 20px;
+  cursor: pointer;
+
+  `;
+const HidePostButton = styled(SlArrowDown)`
+  margin-bottom: 20px;
+  cursor: pointer;
+  
+`;
--- src/Components/MainScreenPage/RenderMessagesContainer.js
+++ src/Components/MainScreenPage/RenderMessagesContainer.js
@@ -1,9 +1,101 @@
-
+import styled from "styled-components";
 
 export default function RenderMessagesContainer() {
 
     return (
-        <>Olaa
-        </>
+        <FeedMessagesContainer>
+            <MessageContainer>
+                <TittleBar>
+                    <h1> TITULO DA MSG</h1>
+                    <ActionButtons>
+                        LALALA
+                    </ActionButtons>
+                </TittleBar>
+                <TextContainer>
+                    <PostInfo>
+                        <h2>@VINII</h2>
+                        <h2>40 ago</h2>
+                    </PostInfo>
+                    <MessageBox>
+                                   ASDASDSADSADSADSADSADSADASDSAD                 
+                    </MessageBox>
+                </TextContainer>
+            </MessageContainer>
+        </FeedMessagesContainer>   
     );
 }
+
+const FeedMessagesContainer = styled.section `
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+    width:100%;
+    height:100%;
+    background-color:lightcoral;
+    padding:24px 0px;
+`
+const MessageContainer = styled.div`
+    display:flex;
+    flex-direction:column;
+    width:90%;
+    background: #FFFFFF;
+    border: 1px solid #999999;
+    border-radius: 16px;
+    margin-bottom:24px;
+`
+const TittleBar = styled.div `
+    display:flex;
+    justify-content:space-between;
+    padding:24px;
+    align-items:center;
+    width:100%;
+    border-radius: 16px 16px 0px 0px;
+    background: #7695EC;
+
+    h1 {
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 22px;
+        line-height: 26px;
+        color: #FFFFFF;   
+    }
+`
+const ActionButtons = styled.div`
+    display:flex;
+    justify-content:space-between;
+
+`
+const TextContainer = styled.div `
+    display:flex;
+    flex-direction:column;
+    padding:24px;
+    width:100%;
+
+`
+
+const PostInfo = styled.div`
+    display:flex;
+    width:100%;
+    justify-content:space-between;
+    margin-bottom:16px;
+
+    h2 {
+        font-family: 'Roboto';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 18px;
+        line-height: 21px;
+        color: #777777;
+        
+    }
+`
+const MessageBox = styled.p`
+    font-family: 'Roboto';
+    width:100%;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 21px;
+    color: #000000;
+`
