e954e38649d96d44794bb71786c1eac1670889a3
EduuCarvalho <educarvalho.engcivil@gmail.com>
2023-05-24 21:12:27 -0300

    feat: get messages done

--- src/Pages/MainScreen.js
+++ src/Pages/MainScreen.js
@@ -1,15 +1,27 @@
 import styled from "styled-components";
 import FixedPostMessageContainer from "../Components/MainScreenPage/FixedPostMessageContainer";
 import RenderMessagesContainer from "../Components/MainScreenPage/RenderMessagesContainer";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function MainScreen() {
+
+    const [messagesList, setMessagesList] = useState();
+
+    useEffect(() =>{
+
+        axios.get("https://dev.codeleap.co.uk/careers/")
+        .then((res)=> {
+            setMessagesList(res.data);
+        })
+        .catch((err)=> console.log(err))
+
+    },[])
 
    return(
         <MainPage> 
             <FixedPostMessageContainer/>
-            <RenderMessagesContainer/>
-        
-
+            <RenderMessagesContainer messagesList={messagesList}/>
         </MainPage>
     );
 };
